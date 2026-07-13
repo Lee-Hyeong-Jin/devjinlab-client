9520f1dbf4f59e926ceb1f43ce0e9ab720a5f063
hyeongjin <lhjin0j@gmail.com>
2026-07-13 15:18:35 +0900

    feat: add semantic header with anchor nav and landing section skeleton
    Replace non-semantic header stub with a Header component (nav/ul/li),
    fix subdomain link typo, and wire about/contact to same-page anchors
    with smooth scroll. Add landing page section skeleton and page metadata.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { Header } from "@/components/header"
 
 export default function Page() {
-  return <div className="flex min-h-svh p-6"></div>
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="hero" className="p-6">
+          {/* TODO: 히어로 콘텐츠 */}
+        </section>
+
+        <section id="about" className="scroll-mt-24 p-6">
+          {/* TODO: about 콘텐츠 */}
+        </section>
+
+        <section id="gallery-preview" className="p-6">
+          {/* TODO: 갤러리 프리뷰, gallery.devjinlab.com 링크 카드 */}
+        </section>
+
+        <section id="blog-preview" className="p-6">
+          {/* TODO: 블로그 프리뷰, blog.devjinlab.com 링크 카드 */}
+        </section>
+
+        <section id="contact" className="scroll-mt-24 p-6">
+          {/* TODO: contact 콘텐츠 */}
+        </section>
+      </main>
+    </>
+  )
 }
